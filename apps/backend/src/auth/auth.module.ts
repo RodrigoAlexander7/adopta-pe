1543f7cad43e5d7fd13eb290477403f6d9db9c84
RodryAlexander <rodrygoleu7@gmail.com>
2025-11-27 21:35:39 -0500

    feat(fix): adding jwt service to auth module

--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { AuthService } from './auth.service';
 import { GoogleStrategy } from './google.strategy';
 import { JwtStrategy } from './jwt.strategy';
 import { UsersModule } from '@/users/users.module';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  providers: [AuthService, GoogleStrategy, JwtStrategy],
+  providers: [AuthService, GoogleStrategy, JwtStrategy, JwtService],
   imports: [UsersModule],
   controllers: [AuthController],
 })
